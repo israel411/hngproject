7a0779181df12c1d454323ca52a36afd0a51f114
israel411 <israelumana1001@gmail.com>
2018-04-26 16:25:45 +0100

    my first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php date_default_timezone_set('Africa/Lagos'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="indexhng.css">
+</head>
+<body>
+   <h1>HOTEL.NG INTERNSHIP 4.0</h1> 
+   <p class="p1">Could your team benefit?</p>
+   <p class="p2"><?php echo date ("h:i:sA"); ?></p>
+   
+
+</body>
+</html>
